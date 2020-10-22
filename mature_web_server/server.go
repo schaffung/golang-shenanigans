9c8050e111ee505c0dfbf7d8619c199f4905b742
srijan-sivakumar <ssivakum@redhat.com>
2020-10-22 09:12:49 +0530

    1. Formatted with go fmt 2. Added a more mature web server

---
+++ mature_web_server/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	// This time we'll create a function to handle the request...like separate
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
